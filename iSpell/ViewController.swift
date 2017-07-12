ed9a05e6ef47d7f62cf4f676363d6dfc1aa3cf3d
Odysseas <odysseasherodotou7@gmail.com>
2017-07-12 22:47:00 +0300

    d

--- iSpell/ViewController.swift
+++ iSpell/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // bc Do any additional setup after loading the view, typically from a nib.
+        // bc   Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
